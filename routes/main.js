d144a93a30e38499a6a07d2d0fef492f9cd154c5
ponyspy <desade4me@gmail.com>
2015-05-03 13:46:35 +0300

    main

--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,21 @@
+var Event = require('../models/main.js').Event;
+
+function mirrorSort(arr) {
+	var result = [];
+	for (var i = 0; i <= arr.length; i++) {
+			if (arr.length == result.length) break;
+			var chunk = arr.splice(0, 3);
+			result = result.concat(chunk.reverse());
+	}
+	return result;
+}
+
+
 exports.index = function(req, res) {
-  res.render('main');
+	News.find().sort('-date').limit(5).exec(function(err, news) {
+		Event.find().sort('-date').limit(6).exec(function(err, events) {
+			var result = mirrorSort(events);
+			res.render('main', {events: result, news: news});
+		});
+	});
 }
