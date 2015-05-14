bce64cc98b42e558838e9409389e9a576bad3c20
plnttr1003 <top@nrdn.ru>
2015-05-14 19:54:51 +0300

    scroll partners

--- public/src/js/scroll.js
+++ public/src/js/scroll.js
@@ -1,4 +1,10 @@
 $(window).load(function(){
+	if ($('.content_scroll_item').length > 0 && $('.content_scroll_item').length < 8) {
+		while ($('.content_scroll_item').length < 15) {
+		$('.content_scroll_item').clone().appendTo('.content_scroll_items');
+		}
+	}
+
 	var parent = $('.content_scroll_items');
 	var max = parent[0].scrollHeight - parent[0].offsetHeight - 20;
 	var scroll_position = 0;
