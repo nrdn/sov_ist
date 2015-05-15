3f4a4b21db7c156e2ce5c1df9c5d2d4d2ce215ad
plnttr1003 <top@nrdn.ru>
2015-05-15 15:13:28 +0300

    team history subsidiarys

--- public/src/js/subsidiarys.js
+++ public/src/js/subsidiarys.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-function sortfil1() {
-	$('.subsidiary_item, .content_items').removeClass('current');
-	$(this).addClass('current');
-	console.log()
-	$('.subsidiary_item[data-fil='+ $(this).attr('data-fil') +']').addClass('current');
-}
-function sortfil2() {
-	$('.subsidiary_item, .content_items').removeClass('current');
-	$(this).addClass('current');
-	$('.content_items[data-fil='+ $(this).attr('data-fil') +']').addClass('current');
-}
-	$('.content_items').on('mouseover', sortfil1);
-	$('.subsidiary_item').on('mouseover', sortfil2);
+	function sortfil1() {
+		$('.subsidiary_item, .content_items').removeClass('current');
+		$(this).addClass('current');
+		console.log()
+		$('.subsidiary_item[data-fil='+ $(this).attr('data-fil') +']').addClass('current');
+	}
+	function sortfil2() {
+		$('.subsidiary_item, .content_items').removeClass('current');
+		$(this).addClass('current');
+		$('.content_items[data-fil='+ $(this).attr('data-fil') +']').addClass('current');
+	}
+		$('.content_items').on('mouseover', sortfil1);
+		$('.subsidiary_item').on('mouseover', sortfil2);
 });
--- public/src/js/team.js
+++ public/src/js/team.js
@@ -11,4 +11,25 @@ $(document).ready(function() {
 	$('.column_main_inner').on('scroll', function(event) {
 		$('.content_scroll_items').scrollTop($(this).scrollTop())
 	});
+
+
+
+
+	function sortfil1() {
+		$('.people_info, .content_item').removeClass('current');
+		$(this).addClass('current');
+		console.log($(this).attr('data-fil'));
+		$('.people_info[data-fil='+ $(this).attr('data-fil') +']').addClass('current');
+	}
+
+	function sortfil2() {
+		$('.people_info, .content_item').removeClass('current');
+		$(this).addClass('current');
+		console.log($(this).attr('data-fil'));
+		$('.content_item[data-fil='+ $(this).attr('data-fil') +']').addClass('current');
+	}
+
+		$('.content_item').on('mouseover', sortfil1);
+		$('.people_info').on('mouseover', sortfil2);
+
 });
