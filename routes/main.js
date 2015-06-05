a05a347d308da0161dda8eba5d8cd7ba9443050a
plnttr1003 <top@nrdn.ru>
2015-06-05 13:27:58 +0300

    subs

--- routes/main.js
+++ routes/main.js
@@ -9,7 +9,7 @@ var __appdir = path.dirname(require.main.filename);
 
 exports.index = function(req, res) {
 	News.find().nor([{'status': 'hidden'}, {'status': 'out'}]).sort('-date').limit(5).exec(function(err, news) {
-		Event.find().nor([{'status': 'hidden'}, {'status': 'out'}]).sort('-date').limit(12).exec(function(err, events) {
+		Event.find().nor([{'status': 'hidden'}, {'status': 'out'}]).sort('-date').limit(12).populate('subsidiary').exec(function(err, events) {
 			res.render('main', {events: events, news: news});
 		});
 	});
@@ -22,7 +22,7 @@ exports.get_events = function(req, res) {
 		? Event.find({'type': {'$in': post.context}}).nor([{'status': 'hidden'}, {'status': 'out'}])
 		: Event.find().nor([{'status': 'hidden'}, {'status': 'out'}]);
 
-	Query.sort('-date').skip(post.skip).limit(post.limit).exec(function(err, events) {
+	Query.sort('-date').skip(post.skip).limit(post.limit).populate('subsidiary').exec(function(err, events) {
 		var opts = {events: events, compileDebug: false, debug: false, cache: true, pretty: false};
 		events.length > 0
 			? res.send(jade.renderFile(__appdir + '/views/events/get_events.jade', opts))
