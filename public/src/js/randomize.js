5a6a186110ef89e3e35563e1b55a9ae855e72456
plnttr1003 <top@nrdn.ru>
2015-07-05 18:43:23 +0300

    main images

---
+++ public/src/js/randomize.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+      $('.content_scroll_items').each(function(){
+            var $scroll_cont = $(this);
+            var $scroll_inn = $scroll_cont.children('.content_scroll_item');
+            $scroll_inn.sort(function(a,b){
+                  var temp = parseInt( Math.random()*10 );
+                  var isOddOrEven = temp%2;
+                  var isPosOrNeg = temp>5 ? 1 : -1;
+                  return( isOddOrEven*isPosOrNeg );
+            })
+            .appendTo($scroll_cont);
+      });
+});
