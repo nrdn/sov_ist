1dbba7770b0d74d15f9287cee1f51429963d0c56
plnttr1003 <top@nrdn.ru>
2015-09-22 20:01:28 +0300

    subs

--- public/src/js/subsidiary.js
+++ public/src/js/subsidiary.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
 			console.log(scrollVal);
 			if (scrollVal > 150) {
 				$('.subsidiary_menu').addClass('fixed');
+				$('.content_title').addClass('fixedtitle');
 			}
 			else {
 				$('.subsidiary_menu').removeClass('fixed');
+				$('.content_title').removeClass('fixedtitle');
 			}
 		})
 });
