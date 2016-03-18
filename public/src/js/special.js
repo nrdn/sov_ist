35fa83c8973b1365e9cd9a3daab9fe8a77d242f3
plnttr1003 <top@nrdn.ru>
2016-03-18 11:07:52 +0300

    specials

--- public/src/js/special.js
+++ public/src/js/special.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-	function nextSlide() {
-		console.log('++++++NEXT++++++')
-		if($(this).index() != ($('.content_photo').length - 1)) {
-			$(this).hide().next().show();
+		function o(e) {
+				console.log("++++++NEXT++++++"),
+				$(this).parent().index() != $(".content_photo").length - 1 ? $(this).parent().hide().next().show() : ($(".content_photo:last-child").hide(),
+				$(".content_photo:first-child").show())
+
 		}
-		else {
-			$('.content_photo:last-child').hide()
-			$('.content_photo:first-child').show()
+		function p(e) {
+				console.log("++++++PREV++++++"),
+				$(this).parent().index() != 0 ? $(this).parent().hide().prev().show() : ($(".content_photo:last-child").show(),
+				$(".content_photo:first-child").hide())
+
 		}
-	}
-	$('.content_photo').on('click',nextSlide);
+		$(".prev_arrow").on("click", p);
+		$(".content_image_hover").on("click", o);
 });
