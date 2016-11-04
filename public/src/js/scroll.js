11477ac7b05fa267246fd8ac1d2fbfd3f62e0930
plnttr1003 <top@nrdn.ru>
2016-11-04 23:13:52 +0300

    slider

--- public/src/js/scroll.js
+++ public/src/js/scroll.js
@@ -1,16 +1,12 @@
 $(window).load(function() {
 
-	function slideConrols(body, popup, popupImage, popupText, el, i, slides) {
+	function slideConrols(dc, body, popup, popupImage, popupText, el, i, slides) {
 		var cross = el('.popup_cross')[0],
 				right = el('.popup_right')[0],
 				left = el('.popup_left')[0],
 				length = slides.length;
 
-		console.log(cross);
-		console.log('i: ' + i + ', length: ' + length);
-
 		function selectSlide(num) {
-			console.log('num: ' + num);
 			popupImage.style.background = 'url(' + slides[i].original + ')';
 			popupText.textContent = slides[i].text;
 		}
@@ -20,17 +16,18 @@ $(window).load(function() {
 		})
 
 		right.addEventListener('click', function() {
-			if (i < length - 1) i++
-			else i = 0
-			selectSlide(i);
+			selectSlide(i < length - 1 ? i++ : i= 0);
 		})
 
 		left.addEventListener('click', function() {
-			if (i > 0) i--
-			else i = length - 1
-			selectSlide(i);
+			selectSlide(i > 0 ? i-- : i = length - 1);
 		})
 
+		dc.onkeydown = function checkKey(e) {
+			if (e.keyCode == '39') selectSlide(i > 0 ? i-- : i = length - 1)
+			else if (e.keyCode == '37') selectSlide(i < length - 1 ? i++ : i= 0)
+			else if (e.keyCode == '27') body.removeChild(popup)
+		}
 	}
 
 	function showSlider() {
@@ -40,14 +37,11 @@ $(window).load(function() {
 				slides = [];
 
 		el('.content_scroll_item').forEach(function(item,i) {
-			console.log(item);
-		 	console.log(item.children.length);
 			slides[i] = {
 									 'path' : item.children.length >= 1 ? item.querySelectorAll('img')[0].getAttribute('src') : item.getAttribute('src'),
 									 'original' : item.children.length >= 1 ? item.querySelectorAll('img')[0].getAttribute('src').replace('thumb.jpg','original.jpg') : item.getAttribute('src').replace('thumb.jpg','original.jpg'),
 									 'text' : item.children.length >= 1 ? item.querySelectorAll('.content_image_description')[0].textContent : ''
 									}
-
 
 			item.addEventListener('click', function() {
 					var popup = document.createElement('div'),
@@ -68,7 +62,7 @@ $(window).load(function() {
 					popup.append(popupImage);
 					body.append(popup);
 
-					slideConrols(body, popup, popupImage, popupText, el, i, slides);
+					slideConrols(dc, body, popup, popupImage, popupText, el, i, slides);
 					});
 			});
 	}
@@ -79,8 +73,8 @@ $(window).load(function() {
 		while ($('.content_scroll_item').length < 12) {
 			$('.content_scroll_item').clone().appendTo('.content_scroll_items');
 		}
-		showSlider();
 	}
+	showSlider();
 
 	var parent = $('.content_scroll_items');
 	var max = parent[0].scrollHeight - parent[0].offsetHeight - 20;
@@ -88,20 +82,20 @@ $(window).load(function() {
 	var factor = 5.55555;
 
 	parent.on('scroll', function(event) {
-	    var s = $(this).scrollTop(),
-	        f = $('>:first', parent),l = $('>:last', parent);
-	    if(s > max) {f.appendTo(parent); parent.scrollTop(s - f.height()); }
-	    if(s < 5) {l.prependTo(parent); parent.scrollTop(s + l.height()); }
+			var s = $(this).scrollTop(),
+					f = $('>:first', parent),l = $('>:last', parent);
+			if(s > max) {f.appendTo(parent); parent.scrollTop(s - f.height()); }
+			if(s < 5) {l.prependTo(parent); parent.scrollTop(s + l.height()); }
 	}).scrollTop(5);
 
 	$('.column_main_inner').on('scroll', function(event) {
-	    var scroll = $(this).scrollTop();
-	    if (scroll_position > scroll) {
-	       parent.scrollTop(parent.scrollTop() - factor);
-	       scroll_position = scroll;
-	    } else {
-	       parent.scrollTop(parent.scrollTop() + factor);
-	       scroll_position = scroll;
-	    }
+			var scroll = $(this).scrollTop();
+			if (scroll_position > scroll) {
+				 parent.scrollTop(parent.scrollTop() - factor);
+				 scroll_position = scroll;
+			} else {
+				 parent.scrollTop(parent.scrollTop() + factor);
+				 scroll_position = scroll;
+			}
 	});
 });
