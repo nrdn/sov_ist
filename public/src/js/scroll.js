16931e3ccf115e50bd6241eb0b700b6cd3f8db0f
plnttr1003 <top@nrdn.ru>
2016-11-04 20:00:22 +0300

    slider

--- public/src/js/scroll.js
+++ public/src/js/scroll.js
@@ -1,13 +1,24 @@
 $(window).load(function() {
 
-	function showslider() {
-		console.log('12');
+	function slideConrols(body, popup, el, i, length) {
+		var cross = el('.popup_cross')[0],
+				right = el('.popup_right')[0],
+				left = el('.popup_left')[0];
+
+		console.log(cross);
+		console.log('i: ' + i + ', length: ' + length);
+
+		cross.addEventListener('click', function() {
+			console.log('22');
+			body.removeChild(popup);
+		})
+	}
+
+	function showSlider() {
 		var dc = document,
 				body = document.getElementsByTagName('body')[0],
 				el = function el(elem){return dc.querySelectorAll(elem)},
 				slides = [];
-
-
 
 		el('.content_scroll_item').forEach(function(item,i) {
 			slides[i] = {
@@ -16,10 +27,11 @@ $(window).load(function() {
 									 'text' : item.querySelectorAll('.content_image_description')[0].textContent
 									}
 			item.addEventListener('click', function() {
-					console.log(slides[i].text);
 					var popup = document.createElement('div'),
 							popupText = document.createElement('div');
 							popupImage = document.createElement('div');
+
+					popup.innerHTML = '<div class="popup_control popup_cross">✕</div><div class="popup_control popup_right">&gt;</div><div class="popup_control popup_left">&lt;</div>';
 
 					popupText.textContent = slides[i].text;
 					popupText.className = "slider_text";
@@ -32,11 +44,45 @@ $(window).load(function() {
 					popupImage.append(popupText);
 					popup.append(popupImage);
 					body.append(popup);
+
+					slideConrols(body, popup, el, i, slides.length);
+
 				});
 			});
 	}
 
 
+
+/*
+
+
+		window.onload = function (argument) {
+			var
+				dc = document,
+				container = dc.getElementsByClassName('container')[0],
+				container_images = container.getElementsByTagName('img'),
+				images_length = container_images.length,
+				i = 0;
+
+			container_images[0].style.display = 'block';
+			dc.onkeydown = checkKey;
+			function checkKey(e) {
+					(function() {
+						var i = images_length;
+							while (i--) {
+									container_images[i].setAttribute("style", "display:none;");
+							}
+					})();
+
+				e = e || window.event;
+				if (e.keyCode == '39') i < images_length - 1 ? i++ : i = 0
+				else if (e.keyCode == '37')  i > 0 ? i-- : i = images_length - 1
+				container_images[i].style.display = 'block'
+			}
+		}
+
+
+*/
 
 
 
@@ -46,7 +92,7 @@ $(window).load(function() {
 		while ($('.content_scroll_item').length < 12) {
 			$('.content_scroll_item').clone().appendTo('.content_scroll_items');
 		}
-		showslider();
+		showSlider();
 	}
 
 	var parent = $('.content_scroll_items');
